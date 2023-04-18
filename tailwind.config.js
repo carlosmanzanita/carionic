bf7e282126a657e72cc2d5b6418e3f2eef9ebdeb
refxxa <100177955+refxxa@users.noreply.github.com>
2023-04-18 17:47:35 -0600

    diseño limpio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts}",
   ],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      'fondo': '#f6f6f6',
-      'elementos': '#272727',
-      'poli': '#75003E',
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': {
-        50: '#fefce8',
-        100: '#ffc82c',
-      },
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
-  }
+    extend: {},
+  },
+  plugins: [],
 }
 
