fec7fd5c998731054a4fa6ed600f1ba4aaf2106d
AldoDanielCM <Aldodaniel24@hotmail.com>
2023-06-29 17:36:20 -0600

    google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
+declare var gtag:any;
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  title = 'codelab-google-analytics';
+  constructor(
+    private router: Router
+  ) 
+  {
+  // const navEndEvents$ =this.router.events
+  //       .pipe(
+  //         filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+  //       )
+  //       navEndEvents$.subscribe((event:NavigationEnd) => {
+  //         gtag('config','G-L37S9CMP1T'{
+  //           'page_path': event.urlAfterRedirects
+  //         });
+  //       });
+  }
 }
